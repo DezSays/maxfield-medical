790ef6627ef09054c9ee7355379ecad2a4ea23a7
Dezarea Bryan <dezareabryan@gmail.com>
2023-05-22 16:43:56 -0400

    Created mimic of desktop.tsx model. Will run testing after implementing routing in issue 3.

--- src/navigation/desktop/desktop.tsx
+++ src/navigation/desktop/desktop.tsx
@@ -2,32 +2,32 @@ import { NavLink } from "react-router-dom";
 import navs from "../../assets/json/navs.json";
 
 const DesktopNavigation = () => {
-  return (
-    <nav className="nav nav--header y-wrap y-wrap--inner">
-      <div className="nav__logo-container">
-        <NavLink className="navs__active" to="/">
-          Home
-        </NavLink>
-      </div>
-      <ul className="nav__navs">
-        {navs.map((navItem) => (
-          <li key={navItem.href}>
-            <NavLink
-              className={navItem.activeClassName}
-              to={navItem.href}
-            >
-              {navItem.value}
-            </NavLink>
-          </li>
-        ))}
-        <li>
-          <NavLink className="navs__active" to="/privacy-policy">
-            Privacy Policy
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="nav nav--header y-wrap y-wrap--inner">
+            <div className="nav__logo-container">
+                <NavLink className="navs__active" to="/">
+                    Home
+                </NavLink>
+            </div>
+            <ul className="nav__navs">
+                {navs.map((navItem) => (
+                    <li key={navItem.href}>
+                        <NavLink
+                            className={navItem.activeClassName}
+                            to={navItem.href}
+                        >
+                            {navItem.value}
+                        </NavLink>
+                    </li>
+                ))}
+                <li>
+                    <NavLink className="navs__active" to="/privacy-policy">
+                        Privacy Policy
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default DesktopNavigation;
