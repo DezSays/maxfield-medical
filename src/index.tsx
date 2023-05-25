8f56f4801b3264983a8a50795894116d4229411a
Dezarea Bryan <dezareabryan@gmail.com>
2023-05-25 18:45:12 -0400

    Connected ViewportProvider from viewportContext so that I could use the viewport functionality in the entire app.

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./app";
 import "./styles/style.scss";
 import { BrowserRouter } from "react-router-dom";
+import { ViewportProvider } from './context/viewportContext';
 
 const container: HTMLElement | null = document.getElementById("root");
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ViewportProvider>
+        <App />
+      </ViewportProvider>
     </BrowserRouter>
   </React.StrictMode>,
   container
