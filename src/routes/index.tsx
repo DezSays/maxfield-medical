8e161f456ecf7a0a8d771772081c9c5856bc34ce
Dezarea Bryan <dezareabryan@gmail.com>
2023-06-01 17:19:08 -0400

    imported the components and created basic routes. Will work on incorporating the dropdown next.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,22 @@ import Location from './location/location';
 import Members from './members/members';
 import UrgentCareServices from './urgentCareServices/urgentCareServices';
 import Error from './error/error'
+import FluCovidRSVTesting from './urgentCareServices/dropdown/fluCovidRSVTesting/fluCovidRSVTesting';
+import SchoolSportsPhysicals from './urgentCareServices/dropdown/schoolSportsPhysicals/schoolSportsPhysicals';
+import ServicesOffered from './urgentCareServices/dropdown/servicesOffered/servicesOffered';
+import Telemedicine from './urgentCareServices/dropdown/telemedicine/telemedicine';
+import UrgentCareVsER from './urgentCareServices/dropdown/urgentCareVsER/urgentCareVsER';
+import Vaccines from './urgentCareServices/dropdown/vaccines/vaccines';
+import Membership from './members/dropdown/membership/membership';
+import InsuranceCarriers from './insuranceBilling/dropdown/insuranceCarriers/insuranceCarriers';
+import PayMyBill from './insuranceBilling/dropdown/payMyBill/payMyBill';
+import SelfPayPricing from './insuranceBilling/dropdown/selfPayPricing/selfPayPricing';
+import Hippa from './forms/dropdown/hippa/hippa';
+import PatientInfo from './forms/dropdown/patientInfo/patientInfo';
+import DotPhysicals from './employerResources/dropdown/dotPhysicals/dotPhysicals';
+import DrugTesting from './employerResources/dropdown/drugTesting/drugTesting';
+import PreemploymentPhysicals from './employerResources/dropdown/preemploymentPhysicals/preemploymentPhysicals';
+import WorkersComp from './employerResources/dropdown/workersComp/workersComp';
 
 const View = (): JSX.Element => {
 
@@ -22,6 +38,22 @@ const View = (): JSX.Element => {
       <Route path="/forms" element={<Forms />} />
       <Route path="/about-us" element={<AboutUs />} />
       <Route path="/contact-us" element={<ContactUs />} />
+      <Route path="/flu-covid-rsv-testing" element={<FluCovidRSVTesting />} />
+      <Route path="/school-sports-physicals" element={<SchoolSportsPhysicals />} />
+      <Route path="/services-offered" element={<ServicesOffered />} />
+      <Route path="/telemedicine" element={<Telemedicine />} />
+      <Route path="/urgent-care-vs-er" element={<UrgentCareVsER />} />
+      <Route path="/vaccines" element={<Vaccines />} />
+      <Route path="/membership" element={<Membership />} />
+      <Route path="/insurance-carriers" element={<InsuranceCarriers />} />
+      <Route path="/pay-my-bill" element={<PayMyBill />} />
+      <Route path="/self-pay-pricing" element={<SelfPayPricing />} />
+      <Route path="/hippa" element={<Hippa />} />
+      <Route path="/patient-info" element={<PatientInfo />} />
+      <Route path="/dot-physicals" element={<DotPhysicals />} />
+      <Route path="/drug-testing" element={<DrugTesting />} />
+      <Route path="/preemployment-physicals" element={<PreemploymentPhysicals />} />
+      <Route path="/workers-comp" element={<WorkersComp />} />
       <Route path="*" element={<Error />} />
     </Routes>
     </>
--- src/routes/urgentCareServices/urgentCareServices.tsx
+++ src/routes/urgentCareServices/urgentCareServices.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import FluCovidRSVTesting from './dropdown/fluCovidRSVTesting/fluCovidRSVTesting'
+import SchoolSportsPhysicals from './dropdown/schoolSportsPhysicals/schoolSportsPhysicals'
+import ServicesOffered from './dropdown/servicesOffered/servicesOffered'
+import Telemedicine from './dropdown/telemedicine/telemedicine'
+import UrgentCareVsER from './dropdown/urgentCareVsER/urgentCareVsER'
+import Vaccines from './dropdown/vaccines/vaccines'
 
 const UrgentCareServices = () => {
+
+  const handleDropdownChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(event.target.value);
+  };
+
   return (
-    <div>Urgent Care Services</div>
+    <select onChange={handleDropdownChange}>
+      <option disabled selected value="">
+        Urgent Care Services
+      </option>
+        <option>
+          Vaccines
+        </option>
+
+    </select>
   )
 }
 
